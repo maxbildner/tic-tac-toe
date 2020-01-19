20ae78fda87e454792621d21b9436fd04e2cc09a
mbildner <mbildner@icloud.com>
2020-01-19 11:08:12 +0800

    write calculateWinner function, prevent of already clicked squares

--- src/board.js
+++ src/board.js
@@ -22,16 +22,21 @@ class Board extends React.Component {
   handleClick(i) {
     // make soft copy of squares array in state (Immutability!!)
     const squares = this.state.squares.slice();
-
-    // modify squares array (the copy not the array in state!)
-    // Avoiding direct datat mutation lets us keep previous versions so we can "time travel"
-    // Also helps build pure components (main benefit)
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
+    
+    // Only change state if value at position i in squares array is null
+    // and no one won the game
+    if (squares[i] === null && calculateWinner(squares) === null) {
+      
+      // Modify squares array (the copy not the array in state!)
+      // Avoiding direct datat mutation lets us keep previous versions so we can "time travel"
+      // Also helps build pure components (main benefit)
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+  
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext
+      });
+    }
   }
 
   renderSquare(i) {
@@ -43,7 +48,16 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+
+    // if there's a winner, display winner
+    if (winner) {
+      status = 'Congrats! Player ' + winner + ' won!';
+    } else {
+      status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -68,3 +82,40 @@ class Board extends React.Component {
 }
 
 export default Board;
+
+
+
+
+// Helper function decides who won
+// Ex. squares =
+// [
+//   'O', null, 'X',
+//   'X', 'X', 'O',
+//   'O', null, null,
+// ]
+// returns 'X', 'O', or null
+function calculateWinner(squares) {
+  const winningLines = [
+    [0, 1, 2],            // nums refer to 'X's, 'O's, or null in squares array
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningLines.length; i++) {
+    // deconstruction/multi var assignment
+    const [a, b, c] = winningLines[i];
+    // i = 0:   a = 0, b = 1, c = 2
+
+    // if all values of a line in squares are the same, we have a winner
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;            // no winner
+}
